d80ed1776bf2a0f923c61965557e2a2a5e0b80d0
Drich Malonga <malongadrich@gmail.com>
2025-10-15 15:55:54 +0100

    mise à jour du backend

--- routes/proprietaires.js
+++ routes/proprietaires.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { verifyToken } from '../middlewares/auth.js'
-import { getProprietaires, addProprietaire, getProprietaireById } from '../controllers/proprietairesController.js'
+import { getProprietaires, addProprietaire, getProprietaireById, updateProprietaire } from '../controllers/proprietairesController.js'
 
 const router = express.Router()
 
 router.get('/', verifyToken, getProprietaires)   // GET /proprietaires + search via ?search=
 router.post('/', verifyToken, addProprietaire)  // POST /proprietaires
 router.get('/:id', verifyToken, getProprietaireById) // GET /proprietaires/1
+router.put('/:id', verifyToken, updateProprietaire)
 
 export default router
